ede19ccea35e8eb85931d397debb3cc5e24ab969
Osvaldo Manieri Junior <manierijunior@gmail.com>
2023-05-30 17:26:07 -0300

    30/05

---
+++ devagram-node-nextjs/pages/api/login.ts
@@ -0,0 +1,27 @@
+import type {NextApiRequest, NextApiResponse} from 'next';
+
+export default (
+
+    req : NextApiRequest,
+    res : NextApiResponse
+
+) => {
+
+    if(req.method === 'POST'){
+        const {login, senha} = req.body;
+
+        if(login === 'admin@admin.com' &&
+            senha === 'Admin@123'){
+
+                res.status(200).json({msg : 'Usuário autenticado com Sucesso'})
+
+            }
+
+            return res.status(405).json({erro : 'Usuário ou senha não encontrado'});
+
+
+    }
+        return res.status(405).json({erro : 'Método informado não é válido'});
+
+
+}
