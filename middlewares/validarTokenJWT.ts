a0bbb99c9b441b87fe978789cc3236c4e33cc945
Osvaldo Manieri Junior <manierijunior@gmail.com>
2023-06-23 17:10:40 -0300

    atualização token

---
+++ middlewares/validarTokenJWT.ts
@@ -0,0 +1,50 @@
+import type { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
+import type { RespostaPadraoMsg } from "../types/RespostaPadraoMsg";
+import jwt, { JwtHeader, JwtPayload } from 'jsonwebtoken';
+
+export const validarTokenJWT = (handler : NextApiHandler) =>
+      (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
+
+            const {MINHA_CHAVE_JWT} = process.env;
+           
+            if(!MINHA_CHAVE_JWT){
+                    return res.status(500).json({erro : 'ENV de chave JWT não informada na execução do projeto.'});
+            }
+
+                if(!req || !req.headers){
+
+                    return res.status(401).json({ erro : 'Não foi possível validar o token de acesso.'});
+                }
+
+
+                if(req.method !== 'OPTIONS'){
+
+                    const authorization = req.headers['authorization'];
+                    if(!authorization) {
+
+                        return res.status(401).json({ erro : 'Não foi possível validar o token de acesso.'});
+                    }
+
+                    const token = authorization.substring(7);
+                    if(!token){
+
+                        return res.status(401).json({ erro : 'Não foi possível validar o token de acesso.'});
+
+                    }
+
+                    const decoded = jwt.verify (token, MINHA_CHAVE_JWT) as JwtPayload;
+                    if(!decoded){
+
+                        return res.status(401).json({ erro : 'Não foi possível validar o token de acesso.'});
+                    }
+
+                    if(!req.query){
+                            req.query = {};
+
+                    }
+
+                    req.query.userId = decoded._._id
+                }
+
+                return handler(req, res);
+        }
