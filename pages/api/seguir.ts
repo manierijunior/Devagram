7c9a1af995cf459aa0eceed6d4f77fa5d20698e5
Osvaldo Manieri Junior <manierijunior@gmail.com>
2023-07-20 09:49:36 -0300

    Conclusão da funcionalidade de seguir

--- pages/api/seguir.ts
+++ pages/api/seguir.ts
@@ -1,92 +1,68 @@
-import type  {NextApiRequest, NextApiResponse} from 'next';
-import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
-import { validarTokenJWT } from '../../middlewares/validarTokenJWT';
+import type {NextApiRequest, NextApiResponse} from 'next';
 import { conectarMongoDB } from '../../middlewares/conectaMongoDB';
-import { UsuarioModel } from '../../models/UsuarioModel';
+import { validarTokenJWT } from '../../middlewares/validarTokenJWT';
 import { SeguidorModel } from '../../models/SeguidorModel';
+import { UsuarioModel } from '../../models/UsuarioModel';
+import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 
- const endpointSeguir = 
-     async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
-
+const endpointSeguir = 
+    async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
     try{
-        
         if(req.method === 'PUT'){
 
+            const {userId, id} = req?.query;
 
-                 const {userId, id} = req?.query;
+            // usuario logado/autenticado = quem esta fazendo as acoes
+            const usuarioLogado = await UsuarioModel.findById(userId);
+            if(!usuarioLogado){
+                return res.status(400).json({erro : 'Usuario logado nao encontrado'});
+            }
 
-                 // usuario logado/ autenticado = quem esta fazendo as ações
+            // id do usuario e ser seguidor - query
+            const usuarioASerSeguido = await UsuarioModel.findById(id);
+            if(!usuarioASerSeguido){
+                return res.status(400).json({ erro : 'Usuario a ser seguido nao encontrado'});
+            }
 
-                const usuarioLogado = await UsuarioModel.findById(userId);
-                if(!usuarioLogado){
-                    return res.status(400).json({erro : 'Usuario logado não encontrado'});
-
-                }
-
-                // id do usuário a ser seguido - query 
-
-                const usuarioASerSeguido = await UsuarioModel.findById(id);
-                if(!usuarioASerSeguido){
-
-                    return res.status(400).json({erro : 'Usuário a ser seguido não encontrado'});
-                }
-
-                // buscar se EU LOGADO sigo ou não esse usuário
-
-                const euJaSigoEsseUsuario = await SeguidorModel
+            // buscar se EU LOGADO sigo ou nao esse usuario
+            const euJaSigoEsseUsuario = await SeguidorModel
                 .find({usuarioId: usuarioLogado._id, usuarioSeguidoId : usuarioASerSeguido._id});
+            if(euJaSigoEsseUsuario && euJaSigoEsseUsuario.length > 0){
+                // sinal que eu ja sigo esse usuario
+                euJaSigoEsseUsuario.forEach(async(e : any) => 
+                    await SeguidorModel.findByIdAndDelete({_id : e._id}));
+                
+                usuarioLogado.seguindo--;
+                await UsuarioModel.findByIdAndUpdate({_id : usuarioLogado._id}, usuarioLogado);
+                usuarioASerSeguido.seguidores--;
+                await UsuarioModel.findByIdAndUpdate({_id : usuarioASerSeguido._id}, usuarioASerSeguido);
 
-                if(euJaSigoEsseUsuario && euJaSigoEsseUsuario.length > 0){
+                return res.status(200).json({msg : 'Deixou de seguir o usuario com sucesso'});
+            }else{
+                // sinal q eu nao sigo esse usuario
+                const seguidor = {
+                    usuarioId : usuarioLogado._id,
+                    usuarioSeguidoId : usuarioASerSeguido._id
+                };
+                await SeguidorModel.create(seguidor);
 
+                // adicionar um seguindo no usuario logado
+                usuarioLogado.seguindo++;
+                await UsuarioModel.findByIdAndUpdate({_id : usuarioLogado._id}, usuarioLogado);
 
-                    // sinal que eu ja sigo esse usuário 
-                        euJaSigoEsseUsuario.forEach(async(e : any) => 
-                            await SeguidorModel.findByIdAndDelete({_id : e._id}));
+                // adicionar um seguidor no usuario seguido
+                usuarioASerSeguido.seguidores++;
+                await UsuarioModel.findByIdAndUpdate({_id : usuarioASerSeguido._id}, usuarioASerSeguido);
 
-
-                        usuarioLogado.seguindo--;
-                        await UsuarioModel.findByIdAndUpdate({_id : usuarioLogado._id}, usuarioLogado);
-                        usuarioASerSeguido.seguidores--;
-                        await UsuarioModel.findByIdAndUpdate({_id : usuarioASerSeguido._id}, usuarioASerSeguido);
-
-                        return res.status(200).json({msg : 'Deixou de seguir o usuário com sucesso'});
-
-
-                }else{
-
-                        // sinal que eu não sigo esse usuário 
-                        const seguidor = {
-
-                                usuarioid : usuarioLogado._id,
-                                usuarioSeguidoId : usuarioASerSeguido._id
-                        };
-
-                        await SeguidorModel.create(seguidor);
-
-                        // adicionar um segundo usuário logado
-
-                         usuarioLogado.seguindo++;
-                        await UsuarioModel.findByIdAndUpdate({_id : usuarioLogado._id}, usuarioLogado);
-
-                       // adicionar um seguidor no usuario seguido 
-                        usuarioASerSeguido.seguidores++;
-                        await UsuarioModel.findByIdAndUpdate({_id : usuarioASerSeguido._id}, usuarioASerSeguido);
-
-                        return res.status(200).json({msg : 'Usuário seguido com sucesso'});
-                }
-
-
+                return res.status(200).json({msg : 'Usuario seguido com sucesso'});
+            }
         }
-
-        return res.status(405).json({erro : 'Método informado não existe'});
-
+        
+        return res.status(405).json({erro : 'Metodo informado nao existe'});
     }catch(e){
-
         console.log(e);
-        return res.status(500).json({erro : 'Não foi possível seguir/desseguir o usuário informado'});
-
+        return res.status(500).json({erro : 'Nao foi possivel seguir/deseguir o usuario informado'});
     }
-
 }
 
 export default validarTokenJWT(conectarMongoDB(endpointSeguir));
